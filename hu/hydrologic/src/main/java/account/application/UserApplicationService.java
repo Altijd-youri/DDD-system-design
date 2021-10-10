9023513ee88a8fffadb8c6c3d28c0e1553a64395
amin <57110542+aminzammou@users.noreply.github.com>
2021-10-10 13:16:18 +0200

    repository staat er op

--- hu/hydrologic/src/main/java/account/application/UserApplicationService.java
+++ hu/hydrologic/src/main/java/account/application/UserApplicationService.java
@@ -8,7 +8,7 @@ public class UserApplicationService {
 
     public void userAddSavedLocation(String langitude, String longitude, String name, String userID) {
         User user = userRepository.getUser(userID);
-
+        user.createSavedLocation(langitude,longitude,name);
         userRepository.update(user);
     }
 }
