a1cad164d1ca35ab311c5abec130779af8c0eca8
ConsolePeasant92 <hugocornel@yahoo.com>
2021-10-11 12:36:32 +0200

    COMMIT DESCRIPTION

--- hu/hydrologic/src/main/java/account/application/UserApplicationService.java
+++ hu/hydrologic/src/main/java/account/application/UserApplicationService.java
@@ -1,13 +1,11 @@
 package account.application;
 
-import account.domain.CompanyID;
-import account.domain.User;
-import account.domain.UserID;
-import account.domain.UserRepository;
+import account.domain.*;
 import account.port.adapter.persistence.MemoryUserRepository;
 
 public class UserApplicationService {
     private final UserRepository userRepository = new MemoryUserRepository();
+    private final CompanyRepository companyRepository = new MemoryCompanyRepository();
 
     public void userAddSavedLocation(String latitude, String longitude, String name, String userID) {
         User user = userRepository.get(new UserID(userID));
@@ -17,7 +15,11 @@ public class UserApplicationService {
   
     public void addUserToCompany(String userID, String companyID) {
           User user = userRepository.get(new UserID(userID));
-          user.setCompany(new CompanyID(companyID));
-          userRepository.update(user);
+          Company company = companyRepository.get(new CompanyID(companyID));
+
+          if(user != null && company != null) {
+              user.setCompany(new CompanyID(companyID));
+              userRepository.update(user);
+          }
       }
 }
