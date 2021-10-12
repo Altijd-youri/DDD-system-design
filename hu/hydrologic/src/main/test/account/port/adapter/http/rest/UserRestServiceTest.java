ef3d4a9716302d32ec188006a04db9cf02c381c0
ConsolePeasant92 <hugocornel@yahoo.com>
2021-10-12 11:11:30 +0200

    C O M M I T

--- hu/hydrologic/src/main/test/account/port/adapter/http/rest/UserRestServiceTest.java
+++ hu/hydrologic/src/main/test/account/port/adapter/http/rest/UserRestServiceTest.java
@@ -3,32 +3,45 @@ package account.port.adapter.http.rest;
 import account.domain.*;
 import account.port.adapter.persistence.MemoryCompanyRepository;
 import account.port.adapter.persistence.MemoryUserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserRestServiceTest {
+    final private UserRestService userRestService = new UserRestService();
+    final private UserRepository userRepository = new MemoryUserRepository();
+    final private CompanyRepository companyRepository = new MemoryCompanyRepository();
+
+    @BeforeEach
+    void before() {
+        userRepository.clear();
+        companyRepository.clear();
+    }
+
     @Test
     void userAddSavedLocation() {
         //assertThrows(Exception.class,()->testMethod());
     }
 
     @Test
-    public void addUserToCompany() throws Exception {
-        final UserRestService userRestService = new UserRestService();
-        final UserRepository userRepository = new MemoryUserRepository();
-        final CompanyRepository companyRepository = new MemoryCompanyRepository();
+    public void addUserToCompany_isUserExisting() {
+        assertThrows(UserException.class, () -> userRestService.addUserToCompany("user123", "google"));
+        userRepository.store(new User(new UserID("user321"), null, null, null, null, null));
+        assertThrows(UserException.class, () -> userRestService.addUserToCompany("user123", "google"));
+    }
 
-        userRepository.store(new User(new UserID("user123"), null, null, null, null, null));
-        companyRepository.store(new Company(new CompanyID("google"), null, null, null));
+    @Test
+    public void addUserToCompany_isCompanyExisting() {
+        assertThrows(CompanyException.class, () -> userRestService.addUserToCompany("user123", "google"));
+        companyRepository.store(new Company(new CompanyID("microsoft"), null, null, null));
+        assertThrows(CompanyException.class, () -> userRestService.addUserToCompany("user123", "google"));
+    }
 
-        assertThrows(Exception.class, () -> userRestService.addUserToCompany("user123", "microsoft"));
-        assertThrows(Exception.class, () -> userRestService.addUserToCompany("user321", "google"));
-
+    @Test
+    public void addUserToCompany_isUserAdded() throws Exception {
         userRestService.addUserToCompany("user123", "google");
-
-        assertEquals(userRepository.get(new UserID("user123")).getID().toString(), "user123");
         assertEquals(userRepository.get(new UserID("user123")).getCompany().toString(), "google");
     }
 }
